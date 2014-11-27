cc64385c1adc799620fece0e31415f7d7801c5d0
Rafael George <george.rafael@gmail.com>
2014-11-27 19:04:02 -0400

    Rename label from username to Last.fm username to show intend of what is expected

--- spec/features/list_of_concerts_for_a_given_user_spec.rb
+++ spec/features/list_of_concerts_for_a_given_user_spec.rb
@@ -4,7 +4,7 @@ feature 'List five concerts for a given user based on top last.fm ratings' do
   scenario 'Unsuccessful' do 
     visit root_path
 
-    fill_in 'Username', with: 'anonymous' 
+    fill_in 'Last.fm username', with: 'anonymous' 
     click_on 'Give me!'
 
     expect(page).to have_css 'h2', text: "You need to start scrobbling your music preferences so we can help you out"
