633b3f7d75ede39c49aeadcf9fe55e056c221e1c
Rafael George <george.rafael@gmail.com>
2014-12-07 17:43:22 -0400

    Rename concert_url to be concert_uri

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,6 +1,6 @@
 class Concert < ActiveRecord::Base
 
-  def self.save_concert(artist_name, concert_name, concert_url) 
-    create(artist: artist_name, name: concert_name, uri: concert_url)
+  def self.save_concert(artist_name, concert_name, concert_uri) 
+    create(artist: artist_name, name: concert_name, uri: concert_uri)
   end
 end
