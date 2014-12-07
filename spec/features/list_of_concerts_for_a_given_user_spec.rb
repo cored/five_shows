778fe8d47f34e0053d9ed9382efb1957ac48577e
Rafael George <george.rafael@gmail.com>
2014-12-07 13:52:57 -0400

    Fix failing specs for retrieving top artists from last.fm

--- spec/features/list_of_concerts_for_a_given_user_spec.rb
+++ spec/features/list_of_concerts_for_a_given_user_spec.rb
@@ -1,14 +1,15 @@
-require 'vcr_helper'
 require 'rails_helper' 
+require 'vcr_helper'
 
 feature 'List five concerts for a given user based on top last.fm ratings' do 
   scenario 'Unsuccessful' do 
     visit root_path
 
-    fill_in 'Last.fm username', with: 'anonymous' 
-    click_on 'Give me!'
-
     VCR.use_cassette('no-artists-return') do 
+      fill_in 'Last.fm username', with: 'anonymous' 
+      click_on 'Give me!'
+
+
       expect(page).to have_css 'h2', text: "You need to start scrobbling your music preferences so we can help you out"
     end
   end
