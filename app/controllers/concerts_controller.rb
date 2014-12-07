7159574518af164cd26bc6142b4e0a31e4846548
Rafael George <george.rafael@gmail.com>
2014-12-07 17:56:50 -0400

    Add successfully list of events for a given user

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,22 +1,28 @@
 require_relative '../five_shows/last_fm/retrieve_top_artists'
+require_relative '../five_shows/song_kick/retrieve_concerts_for'
 
 class ConcertsController < ApplicationController
   def new
     @concert = Concert.new
   end
 
-  def index 
+  def index
+    @concerts = Concert.all
   end
 
   def create
-    @concerts = FiveShows::LastFm::RetrieveTopArtists.(concerts_params[:username])
-      
+    FiveShows::SongKick::RetrieveConcertsFor.(
+      FiveShows::LastFm::RetrieveTopArtists.(concerts_params[:username])
+    ).map do |concert|
+      Concert.save_concert(concert.artist.name, concert.name, concert.uri)
+    end
+
     redirect_to concerts_path
   end
 
-  private 
+  private
 
-  def concerts_params 
+  def concerts_params
     params.require(:concert).permit(:username)
   end
 end
--- spec/features/list_of_concerts_for_a_given_user_spec.rb
+++ spec/features/list_of_concerts_for_a_given_user_spec.rb
@@ -5,10 +5,9 @@ feature 'List five concerts for a given user based on top last.fm ratings' do
   scenario 'Unsuccessful' do 
     visit root_path
 
-    VCR.use_cassette('no-artists-return') do 
-      fill_in 'Last.fm username', with: 'anonymous' 
+    VCR.use_cassette('no-concerts-return') do 
+      fill_in 'Last.fm username', with: 'dump-anonymous' 
       click_on 'Give me!'
-
 
       expect(page).to have_css 'h2', text: "You need to start scrobbling your music preferences so we can help you out"
     end
@@ -21,7 +20,10 @@ feature 'List five concerts for a given user based on top last.fm ratings' do
       fill_in 'Last.fm username', with: 'cored'
       click_on 'Give me!'
 
-      expect(page).to have_css '.concerts li', text: 'Info'
+      within(:css, 'ul.concerts') do 
+        expect(page).to have_css 'li', text: '#'
+      end
     end
+
   end
 end
