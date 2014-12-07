f60b274a6ca36ce317f866e55e9f47b733a91291
Rafael George <george.rafael@gmail.com>
2014-12-07 17:18:59 -0400

    Add proper handling for no venues returned

--- app/five_shows/song_kick/retrieve_concerts_for.rb
+++ app/five_shows/song_kick/retrieve_concerts_for.rb
@@ -7,12 +7,25 @@ module FiveShows
         songkick_api = Songkickr::Remote.new ENV.fetch('SONGKICK_APIKEY')
         concerts = artists.map do |artist| 
           songkick_result = songkick_api.events(artist.name).results.first
-          Venue.new(artist, songkick_result.display_name, songkick_result.uri)
+          unless songkick_result.nil?
+            Venue.new(artist, songkick_result.display_name, songkick_result.uri)
+          else 
+            NoVenue.new(artist)
+          end
         end
         concerts
       end
 
       Venue = Struct.new(:artist, :name, :uri)
+
+      class NoVenue
+        attr_reader :artist, :name, :uri
+        def initialize(artist)
+          @artist = artist 
+          @name = "No Venue" 
+          @uri = "#"
+        end
+      end
     end
   end
 end
--- spec/five_shows/song_kick/retrieve_concerts_for_spec.rb
+++ spec/five_shows/song_kick/retrieve_concerts_for_spec.rb
@@ -6,25 +6,42 @@ require_relative '../../../app/five_shows/song_kick/retrieve_concerts_for'
 
 describe FiveShows::SongKick::RetrieveConcertsFor do
   describe '#call' do
-    let(:artists) do
-      ['The Used', 
-       'Ed Sheeran', 
-       'La Roux',
-       'The Cat Empire', 
-       'Kaiser Chiefs'].map do |name|
-          FiveShows::LastFm::RetrieveTopArtists::Artist.new(name, 1, '#')
-       end
+    context 'when successfully returned venues' do 
+      let(:artists) do
+        ['The Used', 
+         'Ed Sheeran', 
+         'La Roux',
+         'The Cat Empire', 
+         'Kaiser Chiefs'].map do |name|
+           FiveShows::LastFm::RetrieveTopArtists::Artist.new(name, 1, '#')
+         end
+      end
+
+      it 'returns a list of urls for concerts' do
+        VCR.use_cassette('songkick-events') do 
+          expect(
+            FiveShows::SongKick::RetrieveConcertsFor.(artists).map(&:uri)
+          ).to eql(["http://www.songkick.com/concerts/22276903-used-at-mo-club?utm_source=31538&utm_medium=partner", 
+                    "http://www.songkick.com/festivals/1184053/id/22197803-capitals-jingle-bell-ball-2014?utm_source=31538&utm_medium=partner", 
+                    "http://www.songkick.com/concerts/20706858-la-roux-at-gibson?utm_source=31538&utm_medium=partner", 
+                    "http://www.songkick.com/festivals/122786/id/22047523-woodford-folk-festival-2014?utm_source=31538&utm_medium=partner", 
+                    "http://www.songkick.com/concerts/20968878-foo-fighters-at-cape-town-stadium?utm_source=31538&utm_medium=partner"])
+        end
+      end
     end
 
-    it 'returns a list of urls for concerts' do
-      VCR.use_cassette('songkick-events') do 
-        expect(
-          FiveShows::SongKick::RetrieveConcertsFor.(artists).map(&:uri)
-        ).to eql(["http://www.songkick.com/concerts/22276903-used-at-mo-club?utm_source=31538&utm_medium=partner", 
-                  "http://www.songkick.com/festivals/1184053/id/22197803-capitals-jingle-bell-ball-2014?utm_source=31538&utm_medium=partner", 
-                  "http://www.songkick.com/concerts/20706858-la-roux-at-gibson?utm_source=31538&utm_medium=partner", 
-                  "http://www.songkick.com/festivals/122786/id/22047523-woodford-folk-festival-2014?utm_source=31538&utm_medium=partner", 
-                  "http://www.songkick.com/concerts/20968878-foo-fighters-at-cape-town-stadium?utm_source=31538&utm_medium=partner"])
+    context 'when there are no venues' do 
+      let(:artists) do
+        ['Glassjaw'].map do |name|
+          FiveShows::LastFm::RetrieveTopArtists::Artist.new(name, 1, '#')
+        end
+      end
+
+      it 'handles nil returns from song kick' do 
+        VCR.use_cassette('songkick-events-with-nils') do 
+          expect(FiveShows::SongKick::RetrieveConcertsFor.(artists).map(&:uri)
+                ).to eql(["#"])
+        end
       end
     end
   end
