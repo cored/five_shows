91198ae0c8e6b4912e33d463b301663a77cb3d1e
cored <george.rafael@gmail.com>
2015-03-31 17:56:35 -0400

    Add #last_fm method to handle the api initialization

--- app/five_shows/last_fm/retrieve_top_artists.rb
+++ app/five_shows/last_fm/retrieve_top_artists.rb
@@ -10,8 +10,6 @@ module FiveShows
       end
 
       def retrieve
-        last_fm = Lastfm.new(ENV.fetch('LASTFM_API_KEY'),
-                             ENV.fetch('LASTFM_SECRET_KEY'))
         last_fm.user.get_top_artists(user: username).map do |artist| 
           Artist.new(artist.fetch("name"),
                      artist.fetch("playcount"),
@@ -24,6 +22,11 @@ module FiveShows
       Artist = Struct.new(:name, :playcount, :url)
 
       private 
+
+      def last_fm
+        @last_fm ||= Lastfm.new(ENV.fetch('LASTFM_API_KEY'), 
+                                ENV.fetch('LASTFM_SECRET_KEY'))
+      end
 
       attr_reader :username
     end
